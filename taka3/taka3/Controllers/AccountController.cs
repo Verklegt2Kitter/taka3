72902814193bf75d7c580b91547cf5ee096c52e8
Ingvi77 <ingvi13@ru.is>
2015-05-08 12:24:25 +0000

    UserLogin update Ingvi

--- taka3/taka3/Controllers/AccountController.cs
+++ taka3/taka3/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace taka3.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName, BirthDate = model.BirthDate, Country = model.Country }; //bætti við Frá Email Ingvi 
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
